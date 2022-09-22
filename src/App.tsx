6f3ab20454f34c51d1adcfe69dc8ef8979624c5c
gxc <1003823149@qq.com>
2022-09-22 09:54:49 +0800

    添加ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import {Button} from 'antd';
+import { Button } from 'antd';
+import { Header } from './components/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Button type='primary'>点击</Button>
     </div>
   );
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Header() {
+    return (
+        <div className='header-module'>
+            头部
+        </div>
+    );
+}
